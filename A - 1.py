bb8b5064b3383d5b73718d1703fe1c7290fb8538
Samarsh7520 <samarsh.jaiz@gmail.com>
2026-05-17 17:32:14 +0530

    Add files via upload

---
+++ A - 1.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 4
+
+# Using AND operator(MULTIPLY)
+print("num1 and num2 = ", num1 & num2)
+
+# Using OR operator
+print("\nnum1 | num2 = ", num1 | num2)
+
+# Using NOT operator
+print("\n~num1 = ", ~num1)
+
+# Using XOR operator
+print("\nnum1 ^ num2 = ", num1 ^ num2) 
